c2c389032a3b62740df46071d0d48bed8cb30da9
Angel Aguirre <angelaguirrel@hotmail.com>
2023-05-06 22:35:02 -0600

    Add auto slugify on save

--- surveys/models/survey.py
+++ surveys/models/survey.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.exceptions import MultipleObjectsReturned, ObjectDoesNotExist
+from django.utils.text import slugify
 
 
 class Survey(models.Model):
@@ -12,3 +14,24 @@ class Survey(models.Model):
     slug = models.SlugField(max_length=100, unique=True)
     is_active = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        if not len(self.slug.strip()):
+            self.slug = slugify(self.title)
+
+        _slug = self.slug
+        _count = 1
+
+        while True:
+            try:
+                Survey.objects.all().exclude(pk=self.pk).get(slug=_slug)
+            except MultipleObjectsReturned:
+                pass
+            except ObjectDoesNotExist:
+                break
+            _slug = "%s-%s" % (self.slug, _count)
+            _count += 1
+
+        self.slug = _slug
+
+        super(Survey, self).save(*args, **kwargs)

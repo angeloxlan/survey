2d25eca1d7751d5bbc4e5007b8ca3bcb0d7893fa
Angel Aguirre <angelaguirrel@hotmail.com>
2023-05-08 21:18:33 -0600

    Refactor the url conf

--- surveys/urls.py
+++ surveys/urls.py
@@ -1,10 +1,14 @@
-from django.urls import path
+from django.urls import path, include
 from django.http import HttpResponse
 
 from .views import create_survey, preview_survey
 
 
+survey_patterns = [
+    path('create/', create_survey, name='create-survey'),
+    path('preview/<slug:slug>/', preview_survey, name='preview-survey'),
+]
+
 urlpatterns = [
-    path('survey/create/', create_survey, name='create-survey'),
-    path('survey/preview/<slug:slug>/', preview_survey, name='preview-survey'),
+    path('survey/', include(survey_patterns)),
 ]

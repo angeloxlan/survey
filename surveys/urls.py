19e61fbdb44ef866a5dc1f1302cd3c4f37125481
Angel Aguirre <angelaguirrel@hotmail.com>
2023-05-10 20:33:01 -0600

    Fix the views import path after the refactor

--- surveys/urls.py
+++ surveys/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path, include
 from django.http import HttpResponse
 
-from .views import create_survey, preview_survey
+from .views import survey
 
 
 survey_patterns = [
-    path('create/', create_survey, name='create-survey'),
-    path('preview/<slug:slug>/', preview_survey, name='preview-survey'),
+    path('create/', survey.create_survey, name='create-survey'),
+    path('preview/<slug:slug>/', survey.preview_survey, name='preview-survey'),
 ]
 
 urlpatterns = [
